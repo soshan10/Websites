1c970d5882ff48a7ec23d7b1382cb9fe191bce39
DESKTOP-92Q2AQQ\soshan <soshan10@gmail.com>
2019-07-17 00:48:37 -0400

    Added socket.io code

--- ChatApp/src/index.js
+++ ChatApp/src/index.js
@@ -1,11 +1,25 @@
 var app = require('express')();
 var httpServer = require('http').createServer(app);
+var path = require('path');
+var io = require('socket.io')(httpServer);
 
 app.get('/', 
     function(req, res){
-        res.send('<h1>Hello world</h1>');
+        //res.send('<h1>Hello world</h1>');
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     }
 );
+
+io.on('connection', function(socket){
+    console.log('a user connected. ID:' + socket.client.id);
+    socket.on('disconnect', function(){
+      console.log('user disconnected. ID:' + socket.client.id);
+    });
+    socket.on('chat message', function(msg){
+        console.log('Chat message ' + msg);
+        io.emit('chat message', msg);
+    })
+  });
 
 httpServer.listen(3000,
     function(){
